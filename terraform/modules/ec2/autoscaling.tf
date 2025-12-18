134b4102ca6e39f90d727b6575dd282a499b21c1
shivsagar07 <161929702+shivsagar07@users.noreply.github.com>
2025-12-18 14:20:03 +0530

    Create autoscaling.tf

---
+++ terraform/modules/ec2/autoscaling.tf
@@ -0,0 +1,18 @@
+resource "aws_autoscaling_group" "app_asg" {
+  desired_capacity     = 2
+  max_size             = 3
+  min_size             = 1
+  vpc_zone_identifier  = var.private_subnets
+  target_group_arns    = [var.target_group_arn]
+
+  launch_template {
+    id      = aws_launch_template.app_lt.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "Health-Insurance-App"
+    propagate_at_launch = true
+  }
+}
