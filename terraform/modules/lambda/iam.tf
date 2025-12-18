ec52c70f270eb508d8bb24d0bf0807edc5dc27d0
shivsagar07 <161929702+shivsagar07@users.noreply.github.com>
2025-12-18 18:54:05 +0530

    Create iam.tf

---
+++ terraform/modules/lambda/iam.tf
@@ -0,0 +1,46 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-cost-optimization-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = { Service = "lambda.amazonaws.com" },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name = "lambda-cost-optimization-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ec2:DescribeInstances",
+          "ec2:StopInstances",
+          "ec2:DescribeSnapshots",
+          "ec2:DeleteSnapshot"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
